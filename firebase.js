91bbbc5dee2ed5676d010eea939f6599a7e15558
Robert Prince <rob23codingbootcamp@gmail.com>
2018-04-09 08:57:57 -0700

    changes

--- firebase.js
+++ firebase.js
@@ -1,5 +1,13 @@
+
+firebase.initializeApp(config);
+var database = firebase.database;
+
 $(document).ready(function(){
 //create variables to retrieve values from the form field
+   $('#submit-button').on('click', function(event){
+
+    //I don't think that event.preventDefault (); is needed here.
+  
 
   var trainName = $('#trainName').val('');
   //now make sure that the form field is cleared
@@ -21,4 +29,9 @@ $(document).ready(function(){
 
   var currentTime = moment();
      console.log("current Time" + moment(currentTime).format("hh:mm"));
+
+     console.log('trainName');
+
+     
+});
 });
